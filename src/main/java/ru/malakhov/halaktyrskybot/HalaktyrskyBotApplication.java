d33d16dbf939ce2cc893f72381c5ea10122ace16
Aleksandr Malakhov <schwarcroza@mail.ru>
2024-05-19 11:22:13 +0600

    first commit

---
+++ src/main/java/ru/malakhov/halaktyrskybot/HalaktyrskyBotApplication.java
@@ -0,0 +1,13 @@
+package ru.malakhov.halaktyrskybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalaktyrskyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalaktyrskyBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/malakhov/halaktyrskybot/HalaktyrskyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.malakhov.halaktyrskybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalaktyrskyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
