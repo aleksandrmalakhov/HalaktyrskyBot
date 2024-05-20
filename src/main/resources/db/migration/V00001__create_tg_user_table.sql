f45258aea9157522a28d6de7a9912d8d3fbadbee
Aleksandr Malakhov <schwarcroza@mail.ru>
2024-05-20 16:06:02 +0600

    add first migration 'create_tg_user_table'

---
+++ src/main/resources/db/migration/V00001__create_tg_user_table.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS tg_user;
+create table tg_user
+(
+    id               int8    not null,
+    first_login_date timestamp,
+    first_name       varchar(255),
+    is_active        boolean not null,
+    last_name        varchar(255),
+    user_name        varchar(255),
+    primary key (id)
+)
